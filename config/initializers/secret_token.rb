69d9cc8a517d9a81688f9aebdb206d2c59804f49
Adam Anderson <adam@makeascene.com>
2012-09-26 22:32:56 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dinnah::Application.config.secret_token = '529076901f96bdaf801c0ff33fb40b5ecd2de6b6f4f8966bde425eb19718d2f3421f1b64724ba931ed8ee35cdc08f790a1187216b45ee881df82199b108a4a86'
